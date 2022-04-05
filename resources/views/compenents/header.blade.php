0c87683d7559e15be90f3a4a066aaac02b3f6610
WafaAlsabri <wafaalsabri23@gmail.com>
2022-04-05 09:42:44 +0300

    add yield pages

---
+++ resources/views/compenents/header.blade.php
@@ -0,0 +1,57 @@
+<div class="container">
+    @yield('header')
+</div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light mk" >
+    <div class="container-fluid ">
+      <h3 class="animate-charcter mx-5"> job</h3>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+        <ul class="navbar-nav ">
+          <li class="nav-item me-5">
+            <a class="nav-link active sizee" aria-current="page" href="index.html"><i class="fas fa-home mx-2"></i>Home</a>
+          </li>
+          <li class="nav-item me-5">
+            <a class="nav-link sizee" href="jobs.html" > <i class="fas fa-user-md mx-2"></i>Jobs</a>
+          </li>
+
+          <li class="nav-item me-5">
+            <a class="nav-link sizee"  href="contact.html"><i class="fas fa-id-card-alt mx-2"></i>Contact</a>
+          </li>
+          <li class="nav-item me-5">
+            <a class="nav-link sizee" href="services.html"> <i class="fas fa-user-md mx-2"></i>Services</a>
+          </li><li class="nav-item me-5">
+            <a class="nav-link sizee" href="about.html"><i class="far fa-address-card mx-2"></i>About</a>
+          </li>
+
+          <li class="nav-item dropdown me-5">
+            <a class="nav-link dropdown-toggle sizee" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="fas fa-user-circle"></i> Account
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="sidebar.html">profile</a></li>
+              <li><a class="dropdown-item" href="#">Sign up</a></li>
+        
+            </ul>
+          </li>
+          <li class="nav-item me-5">
+            <a class="nav-link sizee" href="cv.html"><i class="fas fa-language mx-2"></i>cv</a>
+          </li>
+          <li class="nav-item me-5">
+            <a class="nav-link sizee" href="clints.html"><i class="fas fa-language mx-2"></i>our partners</a>
+          </li>
+          <li class="nav-item me-5">
+            <a class="nav-link sizee" href="index - Copy.html"><i class="fas fa-language mx-2"></i>English</a>
+          </li>
+         <li class="nav-item " >
+            <a class="nav-link" href="signup.html"><button type="button" class="btn btn-secondary ciz">Sign up</button></a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="signin.html"><button type="button" class="btn btn-secondary ciz">Log in</button></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
